8eaed6f8fe41e025fe8eeff78f23b3d77d1608df
Jeffrey Ojuederhie <vgauss07@gmail.com>
2024-03-28 17:16:09 -0400

    updates + config file

---
+++ ID_Tampering_Detection/config.py
@@ -0,0 +1,23 @@
+import os
+from os import environ
+
+class Config(object):
+
+    DEBUG = False
+    TESTING = False
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = 'pianalytix'
+
+    UPLOADS = ""
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+class Development(Config(Config)):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+class DebugConfig(Config):
+    DEBUG = False
